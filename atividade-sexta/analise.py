7d659965fcb57e09d586ccc35175c6104ea9552b
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-04-04 08:40:54 -0400

    Create analise.py

---
+++ atividade-sexta/analise.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+df = pd.read_csv('vgsales.csv') #abrindo e lendo o arquivo
+
+column_names = df.columns
+
+
+# if 3 -> dar opcao de escolher a quantia de jogos que quer listar
+
+# if 2 -> dar a opcao de filtrar as vendas e deixar o usuário escolher o tipo do filtro
+
+# if 4 -> Lancamento de jogos e deixar a pessoa poder escolher o filtro de quando foi lancado (ano)
+
+# if 5 -> mostrar jogos publicados pela nintendo na (a pessoa escolhe eua ou EU sales) acima de 10 milhoes extra: deixar o usuario escolher se quer nintendo ou outra empresa
+
+# if 6 -> pesquisar o jogo por genero
+
+while True:
+
+    print('Digite a opção que deseja:\n1 - Informações sobre o arquivo/dados\n2 - Vendas\n3 - Listar jogos\n4 - Ver lançamentos\n5 - Mostrar jogos publicados')
+
+    op = int(input('- '))
+
+
+    if op == 1:
+        print('Deseja ver o que?\n1 - Titulo das colunas\n2 - Número de linhas e colunas\n3 - #ainda decidindo... ')
+        op = int(input('- '))
+        if op == 1:
+            info = df.info()
+        elif op == 2:
+            print(f'\nNúmero de linhas:c {df.shape[0]} | Número de colunas: {df.shape[1]}\n')
+    if op == 2:
+        print('Como deseja ver?\n1 - Filtrar por ano\n2 - Filtrar por empresa\n3 - Filtrar por número de vendas\n4 - Filtrar por continente\n5 - ver vendas totais')
+        op = int(input('- '))
+        
